4d12ebffd59bdaedb52961ce6accd0a5139b2f6c
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2023-11-27 20:52:52 +0100

    Added 3 mandatory trials and more

--- script.js
+++ script.js
@@ -12,33 +12,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let NQB;
 
+let tryCount = 0;
+let answered = false;
+
 function verifyAnswer() {
+  if (answered) {
+    return;
+  }
+
   const userAnswer = document.getElementById("ans").value;
   const currentQuestionElement = document.getElementById("question");
   const currentQuestion = currentQuestionElement.textContent;
   const correctAnswer = currentQuestionElement.getAttribute("answer");
 
-  if (userAnswer.toLowerCase() == correctAnswer.toLowerCase()) {
+  if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
     console.log("Correct!");
     showAlert("Correct!");
     NQB = createNQB();
     NQB.style.backgroundColor = "#3fb541"; // greenish
+    answered = true;
   } else {
-    console.log("Incorrect!");
-    showAlert("Incorrect!");
-    NQB = createNQB();
-    NQB.style.backgroundColor = "#b53f3f"; // reddish
+    tryCount++;
+    if (tryCount < 3) {
+      console.log("Incorrect! Try again.");
+      showAlert(`Incorrect! Try again. Tries left: ${3 - tryCount}`);
+    } else {
+      console.log("Incorrect! Showing correct answer.");
+      showAlert(`Incorrect! The correct answer is: ${correctAnswer}.`);
+      NQB = createNQB();
+      NQB.style.backgroundColor = "#b53f3f"; // reddish
+      answered = true;
+      document.getElementById("ans").disabled = true;
+      const panel = document.getElementById("panel");
+      clearNQB(panel);
+      panel.appendChild(NQB);
+    }
   }
 
-  const panel = document.getElementById("panel");
-  clearNQB(panel);
-  panel.appendChild(NQB);
+  if (!answered && tryCount === 3) {
+    // If user has exceeded 3 tries, disable the answer input and show the "Next question" button
+    document.getElementById("ans").disabled = true;
+    NQB = createNQB();
+    NQB.style.backgroundColor = "#e65454"; // reddish
+    const panel = document.getElementById("panel");
+    clearNQB(panel);
+    panel.appendChild(NQB);
+  }
+}
+
+function showNQ() {
+  answered = false;
+  tryCount = 0;
+  document.getElementById("ans").disabled = false;
   document.getElementById("ans").value = "";
+  fetch("./questions.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const questionElement = document.getElementById("question");
+      const randomQuestionObj = data[Math.floor(Math.random() * data.length)];
+      const randomQuestion = `${randomQuestionObj.category}: ${randomQuestionObj.question}`;
+      questionElement.textContent = randomQuestion;
+      questionElement.setAttribute("answer", randomQuestionObj.answer);
+    });
 }
 
 function clearNQB(parentElement) {
-  const previousNQB =
-    parentElement.querySelector("#nQ");
+  const previousNQB = parentElement.querySelector("#nQ");
   if (previousNQB) {
     previousNQB.remove();
   }
@@ -54,23 +93,6 @@ function createNQB() {
     hideAlert();
   };
   return NQB;
-}
-
-//   setTimeout(function () {
-//     document.getElementById("ans").value = "";
-//     showNQ();
-//   }, 10000);
-
-function showNQ() {
-  fetch("./questions.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const questionElement = document.getElementById("question");
-      const randomQuestionObj = data[Math.floor(Math.random() * data.length)];
-      const randomQuestion = `${randomQuestionObj.category}: ${randomQuestionObj.question}`;
-      questionElement.textContent = randomQuestion;
-      questionElement.setAttribute("answer", randomQuestionObj.answer);
-    });
 }
 
 let correctAnswers = 0;
@@ -92,12 +114,12 @@ function showAlert(message) {
 }
 
 function hideAlert() {
-  const alertBox = document.querySelector('.alert-box');
+  const alertBox = document.querySelector(".alert-box");
   if (alertBox) {
     alertBox.classList.remove("show");
     setTimeout(() => {
       alertBox.remove();
-    }, 250)
+    }, 250);
   }
 }
 
