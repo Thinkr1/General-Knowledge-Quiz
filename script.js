79168fb4520ab767f21180f5745931c9d5d99adb
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2023-11-28 21:34:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -107,6 +107,8 @@ function formatQuestionText(question) {
   
   function skipQuestion() {
     showNQ();
+    const questionElement = document.getElementById("question");
+    showAlert(questionElement.getAttribute("answer"))
     updateSkippedCount();
   }
   
